9044205db23ab938bb159bd57608129a4c1a77fc
jonjia <jiawenhuiwar3@163.com>
2024-05-26 18:42:08 +0800

    feat: support auto_deref and auto_debug macros by darling

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,13 @@
 
 // for enum, we'd like to generate From impls for each variant
 // for struct we'd like to generate From impls for each field
+mod auto_debug;
+mod auto_deref;
 mod enum_from;
 mod enum_from_darling;
 
+use auto_debug::process_auto_debug;
+use auto_deref::process_auto_deref;
 use enum_from::process_enum_from;
 use enum_from_darling::process_enum_from_darling;
 use proc_macro::TokenStream;
@@ -22,4 +26,18 @@ pub fn derive_enum_from_darling(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     // print!("{:#?}", input);
     process_enum_from_darling(input).into()
+}
+
+#[proc_macro_derive(AutoDeref, attributes(deref))]
+pub fn derive_auto_deref(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    // print!("{:#?}", input);
+    process_auto_deref(input).into()
+}
+
+#[proc_macro_derive(AutoDebug, attributes(debug))]
+pub fn derive_auto_debug(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    // print!("{:#?}", input);
+    process_auto_debug(input).into()
 }
